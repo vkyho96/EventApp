5daa1efac4b5c26d6b753edf0152c738fc2c7983
vkyho96 <vkyho96@gmail.com>
2019-01-19 14:28:06 -0800

    testing out react

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react'
 
 
 class App extends Component {
@@ -6,6 +7,8 @@ class App extends Component {
     return (
       <div >
         <h1>Event-App</h1>
+
+        <Button icon="smile" content="React"/>
       </div>
     );
   }
