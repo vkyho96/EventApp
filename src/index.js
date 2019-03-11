4e625c638d8c53096817aa1f553afec5b83494da
vkyho96 <vkyho96@gmail.com>
2019-03-11 13:04:31 -0700

    3.11.2019

--- src/index.js
+++ src/index.js
@@ -1,28 +1,34 @@
-//motified and configured 'hot module replacement' so no need to referesh to see changeses
-//caveat: sometime it doesn't work :D - just refresh when it something happens
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './app/layout/App';
-import * as serviceWorker from './serviceWorker';
+import registerServiceWorker from './registerServiceWorker';
+import { configureStore } from './app/store/configureStore';
 
-
-
+const store = configureStore();
 
 const rootEl = document.getElementById('root');
 
 let render = () => {
-    ReactDOM.render(<App />, rootEl)
-}
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
+    rootEl
+  );
+};
 
 if (module.hot) {
-    module.hot.accept('./app/layout/App', () => {
-        setTimeout(render)
-    });
+  module.hot.accept('./app/layout/App', () => {
+    setTimeout(render);
+  });
 }
+
 render();
 
-serviceWorker.unregister();
+registerServiceWorker();
